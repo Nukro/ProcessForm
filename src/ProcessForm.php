82bb0e5a0b3f0084a2c0ddde4634f52b3eea5869
Nukro <orkunatasoy1996@live.de>
2016-03-11 13:48:31 +0100

    Create ProcessForm.php
    This is the ProcessForm Class.
    
    It has two methods:
    - addInputfield()
        |-> Adds any kind of Inputfield defined with the parameters($inputfieldtype, $formobject, $label, $attributes = array(), $isrequired = 0, $options = array())
    - render()
        |-> calls the render function of the InputfieldForm Class of Processwire to render the form.

---
+++ src/ProcessForm.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * Wrapper Class for the Form API from Processwire
+ * 
+ * ProcessWire 2.x
+ * Copyright (C) 2010 by Ryan Cramer
+ * Licensed under GNU/GPL v2, see LICENSE.TXT
+ *
+ * 
+ *
+ * http://www.processwire.com
+ * http://www.ryancramer.com
+ * 
+ * @author Orkun Atasoy <orkunatasoy1996@live.de>
+ * 
+ * Getting Started: Place the ProcessForm.php file anywhere inside your template folder and include it for example in the init.php file.
+ * Then you can create a new ProcessForm() inside your templates and start building you Form!
+ *
+ */
+
+class ProcessForm{
+
+	public $inputfieldFormObject;
+	public $inputfields = array(
+			'fieldset' => "InputfieldFieldset", 
+			'radio' => "InputfieldRadios", 
+			'text' => "InputfieldText",
+			'textarea' => "InputfieldTextarea",
+			'checkbox' => "InputfieldCheckbox",
+			'submit' => "InputfieldSubmit",
+			'email' => "InputfieldEmail",
+			'float' => "InputfieldFloat",
+			'hidden' => "InputfieldHidden",
+			'int' => "InputfieldInteger",
+			'markup' => "InputfieldMarkup",
+			'name' => "InputfieldName",
+			'password' => "InputfieldPassword",
+			'select' => "InputfieldSelect",
+			'selectmultiple' => "InputfieldSelectMultiple",
+			'url' => "InputfieldURL",
+	);
+
+
+	/**
+	 * Create a InputfieldForm Object and set it as $inputfieldFormObject Property
+	 * @param string $action  Specifies where to send the form-data when a form is submitted
+	 * @param string $method  Specifies the HTTP method to use when sending form-data
+	 * @param string $name_id Specifies id and name attribute of the form
+	 */
+	function __construct($action = "./", $method = "post", $name_id = "ProcessForm"){
+		$form = wire('modules')->get("InputfieldForm");
+		$form->action = $action;
+		$form->method = $method;
+		$form->attr("id+name", $name_id);
+		
+		$this->inputfieldFormObject = $form;
+	}
+
+
+	/**
+	 * Add any kind of inputfield to your Form object
+	 * @param string  $inputfieldtype type of the inputfield
+	 * @param object  $formobject     to which form object should it be added
+	 * @param string  $label          the label of the inputfield
+	 * @param array   $attributes     some input attributes like "class, id, value, name" etc..
+	 * @param integer $isrequired     1 for required and 0 for non-required
+	 * @param array   $options        add some options for radios, selects, checkboxes or selectmultiple inputs ($value => $title)
+	 */
+	function addInputfield($inputfieldtype, $formobject, $label, $attributes = array(), $isrequired = 0, $options = array()){
+			$field = wire('modules')->get($inputfieldtype);
+			$field->label = __($label);
+
+			$attributes = array_filter($attributes);
+			if (!empty($attributes)) {
+				foreach ($attributes as $key => $value) {
+					$field->attr($key, $value);
+				}
+			}
+
+			$options = array_filter($options);
+			if (!empty($options)) {
+				foreach ($options as $key => $value) {
+				    $field->addOption($key, $value);
+				}
+			}
+
+			$field->required = $isrequired;
+			$formobject->append($field);
+
+			if($inputfieldtype == "InputfieldFieldset"){
+				return $field;
+			}
+	}
+
+
+	/**
+	 * render the formobject
+	 * @return [type] [description]
+	 */
+	function render(){
+		return $this->inputfieldFormObject->render();
+	}
+
+}
+
+
+
+?>
